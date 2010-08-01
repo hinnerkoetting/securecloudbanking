8c13dd5c60bb6e12f2eb2d0324c58d0d37909d77
jan <jan@localhost>
2010-08-01 08:10:13 +0200

    bidirektionale verbindungen

---
+++ src/de/mrx/client/AccountDetailDTO.java
@@ -0,0 +1,18 @@
+package de.mrx.client;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class AccountDetailDTO extends AccountDTO {
+	
+	private List<MoneyTransferDTO> transfers;
+
+	public List<MoneyTransferDTO> getTransfers() {
+		return transfers;
+	}
+
+	public void setTransfers(List<MoneyTransferDTO> transfers) {
+		this.transfers = transfers;
+	}
+
+}
