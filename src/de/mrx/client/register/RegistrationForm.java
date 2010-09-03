bfbbc0a3f2e34c22569b22e826a1d1897c92ad53
Jan <Jan@localhost>
2010-09-03 23:44:03 +0200

    Registration Form UIBinder continued

--- src/de/mrx/client/register/RegistrationForm.java
+++ src/de/mrx/client/register/RegistrationForm.java
@@ -1,23 +1,44 @@
 package de.mrx.client.register;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
 import de.mrx.client.GeneralConstants;
+import de.mrx.client.RegisterService;
+import de.mrx.client.RegisterServiceAsync;
+import de.mrx.client.SCB;
 import de.mrx.client.SCBConstants;
+import de.mrx.client.SCBIdentityDTO;
+import de.mrx.client.SCBMessages;
+import de.mrx.shared.SCBException;
 
+/**
+ * Form for the customer registration process
+ * uses UI-Binder
+ * @see RegistrationForm.ui.xml
+ *
+ */
 public class RegistrationForm extends Composite {
 	interface MyUiBinder extends UiBinder<Widget, RegistrationForm> {
 	}
 	
 	private SCBConstants constants = GWT.create(SCBConstants.class);
+	private SCBMessages messages = GWT.create(SCBMessages.class);
 	private static MyUiBinder uiBinder = GWT.create(MyUiBinder.class);
+	
+	RegisterServiceAsync registerSvc;
 
 	@UiField
 	TextBox city;
@@ -44,16 +65,65 @@ public class RegistrationForm extends Composite {
 	
 	@UiField
 	Anchor agblink;
+	
+	@UiField
+	Button registerBtn;
 
 	public RegistrationForm() {
 		// sets listBox
-		initWidget(uiBinder.createAndBindUi(this));
-		language=new ListBox();
+		initWidget(uiBinder.createAndBindUi(this));		
 		language.addItem(constants.languageGerman());
 		language.addItem(constants.languageEnglish());
 		agblink.setHref(GeneralConstants.AGB_LINK);
 		
 	}
+	
+	@UiHandler("registerBtn")
+	void doRegister(ClickEvent e) {
+		
+		SCBIdentityDTO id = new SCBIdentityDTO(lastName.getText());
+		id.setStreet(street.getText());
+		id.setCity(city.getText());
+		id.setPlz(plz.getText());
+		id.setEmail(email.getText());
+		id.setHouseNr(houseNr.getText());
+		id.setFirstName(firstName.getText());
+		
+		if (language.getSelectedIndex()==0){
+			id.setLanguage("de");
+		}
+		else{
+			id.setLanguage("en");
+		}
+		
+		if (registerSvc == null) {
+			registerSvc = GWT.create(RegisterService.class);
+		}
+
+		AsyncCallback<Void> callback = new AsyncCallback<Void>() {
+
+			public void onFailure(Throwable caught) {
+
+				if (caught instanceof SCBException) {					
+					Window.alert(messages.scbError(caught.getMessage()));
+				} else {
+					caught.printStackTrace();
+					Window.alert(messages.registrationError( caught.getMessage()));					
+				}
+
+			}
+
+			public void onSuccess(Void result) {				
+				RootPanel.get(SCB.PAGEID_CONTENT).clear();
+				
+			}
+		};
+
+		registerSvc.register(id, callback);
+
+		
+	    
+	  }
 
 	public TextBox getCity() {
 		return city;
