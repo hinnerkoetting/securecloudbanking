d6b293f169605058830549a95935f073cc94ae97
Hinni <Hinni@localhost>
2010-09-12 21:00:53 +0200

    Changed admin page to be designed by uibinder fixes in registration form

--- src/de/mrx/client/admin/Admin.java
+++ src/de/mrx/client/admin/Admin.java
@@ -11,19 +11,30 @@ package de.mrx.client.admin;
 
 import java.util.List;
 
+import com.allen_sauer.gwt.log.client.Log;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import de.mrx.client.AccountDTO;
 import de.mrx.client.BankDTO;
+import de.mrx.client.CustomerService;
+import de.mrx.client.CustomerServiceAsync;
 import de.mrx.client.MoneyTransferDTO;
-import de.mrx.client.SCB;
+import de.mrx.client.Observable;
+import de.mrx.client.Observer;
+import de.mrx.client.SCBIdentityDTO;
+import de.mrx.client.SCBMenu;
 import de.mrx.client.TransferHistoryForm;
 import de.mrx.client.admin.forms.AccountOverview;
 import de.mrx.client.admin.forms.AdminExternalBanks;
@@ -35,16 +46,40 @@ import de.mrx.client.admin.forms.ExternalAccountOverview;
 import de.mrx.client.admin.forms.NewBank;
 
 
-public class Admin implements EntryPoint {
+public class Admin extends Composite implements EntryPoint,Observer {
+
+	private static AdminUiBinder uiBinder = GWT
+		.create(AdminUiBinder.class);
+
+	interface AdminUiBinder extends
+		UiBinder<Widget, Admin> {
+	}
+
 
 	AdminConstants constants = GWT.create(AdminConstants.class);
 	
 	public final String ADMIN_PAGE_ID = "admin";
 	
 	
-	//test
-	private VerticalPanel page;
-	private Grid pageGrid;
+
+	SCBMenu scbMenu;
+	
+	@UiField
+	HorizontalPanel topMenuPanel;
+	
+	String currentLanguage = "de";
+	
+	@UiField
+	Anchor signIn;
+	
+	@UiField
+	Anchor signOut;
+	
+	@UiField
+	SimplePanel adminMenu;
+	
+	@UiField 
+	SimplePanel content;
 	
     /**
      * show all transactions
@@ -60,6 +95,27 @@ public class Admin implements EntryPoint {
 	
 	public void showEditBankDetails(String name, String blz) {
 		setContent(new EditBankDetails(this, blz, name));
+	}
+	
+	/*
+	 * change the language during runtime keeps the debug flag
+	 */
+	public static void changeToLocalisedVersion(String language) {
+		String queryPart = Window.Location.getQueryString();
+
+		String reloadURL;
+		String debugFlag = Window.Location.getParameter("gwt.codesvr");
+
+		if (debugFlag != null) {
+			reloadURL = GWT.getHostPageBaseURL() +"?gwt.codesvr="+ debugFlag + "&locale="
+					+ language;
+		} else {
+			reloadURL = GWT.getHostPageBaseURL() + "?locale=" + language;
+		}
+
+		GWT.log(reloadURL);
+
+		Window.open(reloadURL, "_self", null);
 	}
 	
 	public void showExternalBanks() {
@@ -149,35 +205,99 @@ public class Admin implements EntryPoint {
 	 * set content of the admin page
 	 */
 	private void setContent(Widget widget) {
-		pageGrid.setWidget(0, 1, widget);
+		content.setWidget(widget);
 	}
 	
 	
+	private void checkGoogleStatus() {
+		CustomerServiceAsync bankingService = GWT.create(CustomerService.class);
+		bankingService.login(GWT.getHostPageBaseURL(),
+				new AsyncCallback<SCBIdentityDTO>() {
+
+					@Override
+					public void onFailure(Throwable caught) {
+						 GWT.log(caught.toString());
+						
+					}
+
+					@Override
+					public void onSuccess(SCBIdentityDTO result) {
+						SCBIdentityDTO identityInfo = result;
+						if (identityInfo.isLoggedIn()) {
+							
+							Log.info("User is logged in with email-adress "
+									+ result.getEmail());
+							if (identityInfo.isActivated()) {								
+								doShowPageForActivatedUser();
+							} else {
+								Log.info("Account not yet activated in SCB: "
+										+ identityInfo);
+								scbMenu.getMenuUserInfo().setText(
+										identityInfo.getEmail());
+								
+							}
+//							loadLoggedInSetup();
+							String language = identityInfo.getLanguage();
+							GWT.log("Language: " + language);
+							if (language != null
+									&& !currentLanguage.equals(language)) {
+								changeToLocalisedVersion(language);
+							}
+						} else {
+							Log.info("User is not yet logged in with his Google account");
+					
+							loadLogin();
+						}
+						
+					}
+
+					private void loadLogin() {
+						// Assemble login panel.
+//						signIn.setHref(identityInfo.getLoginUrl());
+//						signIn.setText(constants.signIn());
+						signIn.setVisible(true);
+						signOut.setVisible(false);
+					}
+
+					private void doShowPageForActivatedUser() {
+						// TODO Auto-generated method stub
+						
+					}
+		
+		});
+	}
+	
 	@Override
 	public void onModuleLoad() {
-		
-		RootPanel r = RootPanel.get(ADMIN_PAGE_ID);		
-		if (r == null)
-			return;
-		page = new VerticalPanel();
+		RootLayoutPanel.get().add(uiBinder.createAndBindUi(this));
 
-			
-		pageGrid = new Grid(1, 2 );
-		page.add(pageGrid);
-		r.add(page);
 		
+		scbMenu = new SCBMenu();
+		scbMenu.addObserver(this);
+		topMenuPanel.add(scbMenu);
+		adminMenu.add(new Adminmenu(this));
+		content.add(new AdminWelcome());
+//	
+//		checkGoogleStatus();
 		
-		pageGrid.setWidget(0, 0, new Adminmenu(this));
-		pageGrid.setWidget(0, 1, new AdminWelcome());
-		
-		//temp workaround for header
-		SCB scb = new SCB();
-		scb.createMainPanel();
-		r = RootPanel.get(SCB.PAGEID_HEADER);
-		r.add(scb.createMainPanel());
-		
-		
+	}
 
+	@Override
+	public void update(Observable source, Object event, Object parameter) {
+		 GWT.log("test");
+		
+	}
+
+	@Override
+	public void reportInfo(String info) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reportError(String error) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
