69d4102f54becc800c25d4160024be90b41dcd37
Hinni <Hinni@localhost>
2010-09-13 18:48:30 +0200

    See last version.

---
+++ src/de/mrx/client/admin/forms/AccountDetails.java
@@ -0,0 +1,147 @@
+package de.mrx.client.admin.forms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.Widget;
+
+import de.mrx.client.AccountDTO;
+import de.mrx.client.MoneyTransferDTO;
+import de.mrx.client.Observable;
+import de.mrx.client.Observer;
+import de.mrx.client.TransferHistoryForm;
+import de.mrx.client.admin.AdminConstants;
+import de.mrx.client.admin.AdminService;
+import de.mrx.client.admin.AdminServiceAsync;
+
+public class AccountDetails extends Composite implements Observable, Observer{
+
+	private static AccountDetailsUiBinder uiBinder = GWT
+			.create(AccountDetailsUiBinder.class);
+
+	interface AccountDetailsUiBinder extends UiBinder<Widget, AccountDetails> {
+	}
+	@UiField
+	SimplePanel content;
+	
+	@UiField
+	Button delete;
+	
+	@UiField
+	Button enableTransfer;
+	
+	@UiField
+	Button enableTransactions;
+	
+	public final static int ACCOUNT_DELETED = 20;
+	
+	public void showTransactions(List<MoneyTransferDTO> result) {
+		content.setWidget(new TransferHistoryForm(result));
+	}
+	
+	AdminConstants constants = GWT.create(AdminConstants.class);
+	
+	AccountDTO account;
+	
+	List<Observer> observer;
+	
+	public AccountDetails(AccountDTO account) {		
+		observer = new ArrayList<Observer>();
+		this.account = account;
+		initWidget(uiBinder.createAndBindUi(this));
+		
+		enableTransfer.setText(constants.transfer());
+		enableTransactions.setText(constants.transactions());
+		delete.setText(constants.delete());
+		clickOnTransfer(null);
+	
+		
+	}
+
+	@UiHandler("enableTransactions")
+	public void clickOnTransfer(ClickEvent e) {
+		AdminServiceAsync service = GWT.create(AdminService.class);
+		service.getTransaction(account.getAccountNr(), new AsyncCallback<List<MoneyTransferDTO>>() {
+
+			@Override
+			public void onFailure(Throwable caught) {
+				GWT.log(caught.toString());											
+			}
+
+			@Override
+			public void onSuccess(List<MoneyTransferDTO> result) {
+				showTransactions(result);							
+			}
+		});
+	}
+	
+	
+	@UiHandler("enableTransfer")
+	public void clickOnTransactions(ClickEvent e) {
+		AdminTransfer adminTransfer = new AdminTransfer(account.getAccountNr(), account.getOwner());
+		adminTransfer.addObserver(this);
+		content.setWidget(adminTransfer);
+		
+	}
+
+	@UiHandler("delete")
+	public void clickOnDelete(ClickEvent e) {
+	if (Window.confirm(constants.deleteAccountConfirm())) {
+		AdminServiceAsync adminService = GWT.create(AdminService.class);
+		adminService.deleteInternalAccount(account.getAccountNr(), new AsyncCallback<String>() {
+
+			@Override
+			public void onFailure(Throwable caught) {
+				GWT.log(caught.toString());								
+			}
+
+			@Override
+			public void onSuccess(String result) {
+				GWT.log(result);
+				notifyObservers(ACCOUNT_DELETED, null);
+				
+			}
+		});
+	}
+}
+
+	@Override
+	public void addObserver(Observer o) {
+		observer.add(o);
+		
+	}
+
+	@Override
+	public void notifyObservers(Integer eventType, Object parameter) {
+		for (Observer o: observer)
+			o.update(this,eventType, parameter);
+	}
+
+	@Override
+	public void update(Observable source, Object event, Object parameter) {
+		notifyObservers((Integer)event, parameter);
+		
+	}
+
+	@Override
+	public void reportInfo(String info) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reportError(String error) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/de/mrx/client/admin/forms/SearchAccountsForm.java
@@ -0,0 +1,247 @@
+package de.mrx.client.admin.forms;
+
+import java.util.List;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.i18n.client.NumberFormat;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
+
+import de.mrx.client.AccountDTO;
+import de.mrx.client.TableStyler;
+import de.mrx.client.admin.AdminConstants;
+import de.mrx.client.admin.AdminService;
+import de.mrx.client.admin.AdminServiceAsync;
+
+public class SearchAccountsForm extends Composite implements UseSearchForm {
+
+	private static SearchAccountsFormUiBinder uiBinder = GWT
+			.create(SearchAccountsFormUiBinder.class);
+
+	interface SearchAccountsFormUiBinder extends UiBinder<Widget, SearchAccountsForm> {
+	}
+	
+	AdminConstants constants = GWT.create(AdminConstants.class);
+	
+	@UiField
+	Button search;
+	
+	@UiField
+	FlexTable overviewTable;
+	
+	@UiField
+	FlexTable selectPages;
+	
+	@UiField
+	TextBox searchOwner;
+	
+	@UiField
+	TextBox searchAccountNr;	
+	
+	private boolean displayName;
+	private boolean displayAccountNr;
+	private boolean displayButton;
+	private boolean displayBalance;
+	private String buttonName = "";
+	
+	private List<AccountDTO> accounts;
+	
+	private final int ACCOUNTS_PER_PAGE = 8;
+	
+//	private final int posTransfer 		= 4;
+//	private final int posDelete 		= 5;
+	
+	
+	//call this form if user clicks on button in searchform
+	UseSearchForm callee;
+	
+	int posOwner;
+	int posAccount;
+	int posBalance;		
+	int posButton;
+	
+	public SearchAccountsForm(UseSearchForm callee) {
+		this.callee = callee;
+		initWidget(uiBinder.createAndBindUi(this));
+		search.setText(constants.search());
+		displayName = true;
+		displayAccountNr = true;
+		displayBalance = true;
+		displayButton = false;
+		
+		posOwner 		= 0;
+		posAccount 		= 1;
+		posBalance 		= 2;		
+		posButton 		= 3;
+		
+	}
+
+	/**
+	 * enables or displays columns
+	 * @param name
+	 * @param accountNr
+	 * @param balance
+	 */
+	public void enableColumns(boolean name, boolean accountNr, boolean balance) {
+		displayName = name;
+		displayAccountNr = accountNr;
+		displayBalance = balance;
+		
+		if (displayName)
+			posOwner = 0;
+		else
+			posOwner = -1;
+		if (displayAccountNr)
+			posAccount = posOwner + 1;
+		else
+			posAccount = posOwner;
+		if (displayBalance)
+			posBalance = posAccount + 1;
+
+		
+	}
+	
+	public void enableButton(boolean enable, String name) {
+		if (enable) {
+			displayButton = true;
+			buttonName = name;
+			posButton = posBalance + 1;
+		}
+		else {
+			displayButton = false;
+		}
+	
+			
+	}
+	
+	
+	public void setAccounts(List<AccountDTO> accounts) {
+		
+		
+		
+		this.accounts = accounts;
+		
+		this.accounts = accounts;
+		
+		overviewTable.clear();
+		
+
+		int numberPages = accounts.size() /  ACCOUNTS_PER_PAGE;
+		if (accounts.size() %  ACCOUNTS_PER_PAGE != 0)
+			numberPages++;
+		
+		for (int i = 1; i <= numberPages; i++) {
+			Anchor pageLink = new Anchor(""+i);
+			final int j = i;
+			pageLink.addClickHandler(new ClickHandler() {
+				
+				@Override
+				public void onClick(ClickEvent event) {
+					switchToPage(j);
+					
+				}
+			});
+			selectPages.setWidget(0, i, pageLink);
+			
+		}
+		
+		
+		
+		
+		//add header
+		if (displayName)
+			overviewTable.setWidget(0, posOwner, new Label(constants.owner()));
+		if (displayAccountNr)
+			overviewTable.setWidget(0, posAccount, new Label(constants.accountNr()));
+		if (displayBalance)
+			overviewTable.setWidget(0, posBalance, new Label(constants.balance()));
+		if (displayButton)
+			overviewTable.setWidget(0, posButton, new Label(buttonName));
+//		overviewTable.setWidget(0, posTransfer, new Label(constants.transferMoney()));
+//		overviewTable.setWidget(0, posDelete, new Label(constants.deleteAccount()));
+		
+		
+		switchToPage(1);
+	}
+	
+	@Override
+	public void clickedOnAccount(AccountDTO account) {
+		callee.clickedOnAccount(account);	
+	}
+
+	private void switchToPage(int page) {
+
+		
+		
+		int row = 1;
+		NumberFormat fmt = NumberFormat.getFormat("#0.00");
+		for (int i = 0; i < ACCOUNTS_PER_PAGE; i++) {
+			int index = (page-1) * ACCOUNTS_PER_PAGE + i;
+			if (index >= accounts.size()) {
+				if (overviewTable.getRowCount() > row)
+					overviewTable.removeRow(row);
+				//row++ is not needed because the last row was just removed
+				continue;
+			}
+			//add all accounts to table
+			final AccountDTO account = accounts.get(index);
+			
+
+			if (displayName)
+				overviewTable.setWidget(row, posOwner, new Label(account.getOwner()));
+			if (displayAccountNr)
+				overviewTable.setWidget(row, posAccount, new Label(account.getAccountNr()));
+			if (displayBalance) {
+				String balance = fmt.format(account.getBalance());
+				overviewTable.setWidget(row, posBalance, new Label(balance));
+			}
+			
+			if (displayButton) {
+				Button button = new Button("x");
+				button.addClickHandler(new ClickHandler() {	
+					public void onClick(ClickEvent event) {
+						callee.clickedOnAccount(account);						
+					}
+				});
+				overviewTable.setWidget(row, posButton, button);
+			}
+		
+			row++;
+		}
+		if ((displayAccountNr | displayBalance | displayButton | displayName) == true) // at least one column enabled
+			TableStyler.setTableStyle(overviewTable);
+		
+	}
+
+	@UiHandler("search")
+	public void onClickSearch(ClickEvent event) {
+	final SearchAccountsForm overview = this;
+	AdminServiceAsync adminService = GWT.create(AdminService.class);
+	
+	adminService.searchInternalAccounts(searchOwner.getText(), searchAccountNr.getText(), new AsyncCallback<List<AccountDTO>>() {
+
+		@Override
+		public void onFailure(Throwable caught) {
+			GWT.log(caught.toString());
+			
+		}
+
+		@Override
+		public void onSuccess(List<AccountDTO> result) {
+			overview.setAccounts(result);
+			
+		}
+	});
+}
+}
