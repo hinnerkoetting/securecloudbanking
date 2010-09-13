88f20f1b33ca32ea5b2e5a894d61ff87899720c0
Hinni <Hinni@localhost>
2010-09-13 11:41:42 +0200

    Added pages for transactions to tables do not get too huge.

--- src/de/mrx/client/TransferHistoryForm.java
+++ src/de/mrx/client/TransferHistoryForm.java
@@ -3,12 +3,16 @@ package de.mrx.client;
 import java.util.List;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
 import com.google.gwt.i18n.client.NumberFormat;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -28,20 +32,50 @@ public class TransferHistoryForm extends Composite {
 	@UiField
 	Label transferHistoryNoTransactionHint;
 	
+	@UiField
+	HorizontalPanel selectPages;
+	
+	private static int TRANSACTIONS_PER_PAGE = 8;
+	private int currentPage = 1;
+	
+	List<MoneyTransferDTO> transfers;
+	
 	/**
 	 * Creates a table of all transfers
 	 * @param transfers to be shown
 	 */
 	public TransferHistoryForm(List<MoneyTransferDTO> transfers) {
+		this.transfers = transfers;
+		
 		//TODO change this
 		SCBConstants constants = GWT.create(SCBConstants.class);
 		
 		
 		initWidget(uiBinder.createAndBindUi(this));
+		
+		
 		if (transfers==null || transfers.size()==0){
 			flexTable.setVisible(false);
 			transferHistoryNoTransactionHint.setVisible(true);
 		}
+		
+		//set pages
+		int numberPages = transfers.size() /  TRANSACTIONS_PER_PAGE + 1;
+		for (int i = 1; i <= numberPages; i++) {
+			Anchor pageLink = new Anchor(""+i);
+			final int j = i;
+			pageLink.addClickHandler(new ClickHandler() {
+				
+				@Override
+				public void onClick(ClickEvent event) {
+					switchToPage(j);
+					
+				}
+			});
+			selectPages.add(pageLink);
+			
+		}
+		
 		
 		//set header labels
 		Label lbl = new Label(constants.accountDetailHeaderDate());
@@ -60,10 +94,29 @@ public class TransferHistoryForm extends Composite {
 		lbl = new Label(constants.accountDetailHeaderAmount());
 		flexTable.setWidget(0, 4, lbl);
 
+		
+		switchToPage(currentPage);
+		
+
+
+	}
+	
+	private void switchToPage(int page) {
+		currentPage = page;
+		//table row
 		int row = 1;
 		
 		NumberFormat fmt = NumberFormat.getFormat("#0.00");
-		for (MoneyTransferDTO transfer: transfers) {
+		Label lbl;
+		for (int i = 0; i < TRANSACTIONS_PER_PAGE; i++) {
+			int index = (page-1) * TRANSACTIONS_PER_PAGE + i;
+			if (index >= transfers.size()) {
+				flexTable.removeRow(row);
+				//row++ is not needed because the last row was just removed
+				continue;
+			}
+			MoneyTransferDTO transfer = transfers.get(index);
+			 
 			//add each transfer
 			lbl = new Label(DateTimeFormat.getMediumDateFormat().format(
 											transfer.getTimestamp()));
@@ -87,10 +140,7 @@ public class TransferHistoryForm extends Composite {
 				flexTable.getCellFormatter().addStyleName(row, 4, "positiveMoney");
 			row++;
 		}
-		
 		TableStyler.setTableStyle(flexTable);
-
-
 	}
 
 }
